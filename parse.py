a0ad9e92c4c5dc4d6b419b45b62930bbe1d00a2f
tUNg131 <halethuy.hlt@gmail.com>
2023-06-02 04:29:56 +0100

    hh

--- parse.py
+++ parse.py
@@ -9,6 +9,9 @@ Classes
 Parser - parses the definition file and builds the logic network.
 """
 
+import itertools
+from scanner import Symbol
+
 
 class Parser:
 
@@ -35,9 +38,86 @@ class Parser:
 
     def __init__(self, names, devices, network, monitors, scanner):
         """Initialise constants."""
+        self._names = names
+        self._devices = devices
+        self._network = network
+        self._monitors = monitors
+        self._scanner = scanner
 
     def parse_network(self):
         """Parse the circuit definition file."""
+
+        device_id = itertools.count(0)
+
+        symbols = []
+        EOF_flag = False
+        while not EOF_flag:
+            while True:
+                sym = self._scanner.get_symbol()
+
+                if sym.type in (Symbol.NL, Symbol.EOF):
+                    EOF_flag = True
+                    break
+                symbols.append(sym)
+
+            first = symbols[0]
+            if first.type == Symbol.NAME:
+                if len(symbols) != 3:
+                    raise RuntimeError
+
+                if symbols[1].type != Symbol.EQUALS:
+                    raise RuntimeError
+
+                if symbols[2].type != Symbol.NAME:
+                    raise RuntimeError
+                
+                first_device_id, first_port_id = self._devices.get_signal_ids(
+                    self._names.get_name_string(first.id))
+                
+                second = symbols[2]
+                second_device_id, second_port_id = self._devices.get_signal_ids(
+                    self._names.get_name_string(second.id))
+
+                self._network.make_connection(
+                    first_device_id, first_port_id, second_device_id, second_port_id)
+            elif first.type == Symbol.DEF:
+                expecting_comma = False
+                for sym in symbols[1:]:
+                    if expecting_comma:
+                        if sym.type != Symbol.COMMA:
+                            raise RuntimeError("Expecting comma")
+                        expecting_comma = False
+                        continue
+
+                    if first.type == Symbol.AND:
+                        self._devices.make_gate(next(device_id), self._devices.AND, sym.data)
+                    elif first.type == Symbol.NAND:
+                        self._devices.make_gate(next(device_id), self._devices.NAND, sym.data)
+                    elif first.type == Symbol.OR:
+                        self._devices.make_gate(next(device_id), self._devices.OR, sym.data)
+                    elif first.type == Symbol.NOR:
+                        self._devices.make_gate(next(device_id), self._devices.NOR, sym.data)
+                    elif first.type == Symbol.XOR:
+                        if first.data != 2:
+                            raise RuntimeError
+                        self._devices.make_gate(next(device_id), self._devices.XOR, 2)
+                    elif first.type == Symbol.CLK:
+                        # Clock_half_period is an integer > 0
+                        if not isinstance(first.data, int) or first.data <= 0:
+                            raise RuntimeError
+                        self._devices.make_clock(next(device_id), self._devices.CLOCK, first.data)
+                    elif first.type == Symbol.SW:
+                        self._devices.make_switch(next(device_id), self._devices.SWITCH, bool(first.data))
+                    elif first.type == Symbol.DTYPE:
+                        self._devices.make_d_type(next(device_id))
+                    else:
+                        raise RuntimeError
+                if not expecting_comma:
+                    raise RuntimeError("end of line has to expect comma")
+            elif first.type == Symbol.MONITOR:
+                pass
+            symbols = []
+
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
--- scanner.py
+++ scanner.py
@@ -9,6 +9,8 @@ Scanner - reads definition file and translates characters into symbols.
 Symbol - encapsulates a symbol and stores its properties.
 """
 
+import re
+
 
 class Symbol:
 
@@ -22,10 +24,9 @@ class Symbol:
     --------------
     No public methods.
     """
-
-    def __init__(self):
+    def __init__(self, type):
         """Initialise symbol properties."""
-        self.type = None
+        self.type = type
         self.id = None
 
 
@@ -48,9 +49,74 @@ class Scanner:
     get_symbol(self): Translates the next sequence of characters into a symbol
                       and returns the symbol.
     """
+    CLOCK = 0
+    SWITCH = 1
+    AND = 2
+    NAND = 3
+    OR = 4
+    NOR = 5
+    DTYPE = 6
+    XOR = 7
+
+    MONITOR = 8
+
+    COMMA = 9
+    EQUAL = 10
+
+    NAME = 11
+    CONNECTION = 12
+
+    EOL = 13
+    EOF = 14
 
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
+        self.names = names
+
+        def gen():
+            with open(path, 'r') as f:
+                for line in f:
+                    for w in line.split(" "):
+                        w = w.lower()
+                        if w[-1] == ',':
+                            yield Symbol(self.get_type(w[:-1]))
+                            yield Symbol(self.COMMA)
+                        else:
+                            yield Symbol(self.get_type(w))
+                    yield Symbol(self.EOL)
+                yield Symbol(self.EOF)
+
+        self.symbols = gen()
+
+    def get_type(self, w):
+        if w == 'clk':
+            return self.CLOCK
+        elif w == 'sw':
+            return self.SWITCH
+        elif w == 'and':
+            return self.AND
+        elif w == 'nand':
+            return self.NAND
+        elif w == 'or':
+            return self.OR
+        elif w == 'nor':
+            return self.NOR
+        elif w == 'dtype':
+            return self.DTYPE
+        elif w == 'xor':
+            return self.XOR
+        elif w == 'mnt':
+            return self.MONITOR
+        elif w == '=':
+            return self.EQUAL
+        # device.output
+        elif re.match(r"^[a-zA-Z_]{1}\w*\.\w*$", w):
+            return self.CONNECTION
+        # device(n)
+        elif re.match(r"^[a-zA-Z_]{1}\w*\(\d+\)$", w):
+            return self.NAME
+        raise RuntimeError(f"Invalid symbol: {w}")
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
+        return next(self.symbols)
